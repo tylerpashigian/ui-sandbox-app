399aed345557996282e068bc3542bf51b1f85b47
Tyler Pashigian <tylerpashigian@gmail.com>
2025-01-21 19:55:18 -0600

    Creating custom animation for popover - adding badge, combobox, command, drawer, popover, input, and sheet shadcn components - added react scan support

--- src/app/popover/page.tsx
+++ src/app/popover/page.tsx
@@ -1,11 +1,88 @@
+"use client";
+
+import { useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+import { AnimatePresence, motion } from "framer-motion";
+
 import FeedbackComponent from "../_components/ticket";
 import FeedbackComponentCSS from "../_components/course/ticket-course";
 
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogPortal,
+  DialogTitle,
+} from "~/components/ui/dialog";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+} from "~/components/ui/drawer";
+
 const PopoverDemo = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const springTransition = { duration: 2.7, type: "spring", bounce: 0.25 };
+
+  const customTrigger = (
+    <motion.div layoutId="customModal" transition={springTransition}>
+      <Button variant="secondary" onClick={() => setIsOpen(true)}>
+        Open Overlay
+      </Button>
+    </motion.div>
+  );
+
   return (
     <div className="flex flex-col gap-4">
       <FeedbackComponentCSS />
       <FeedbackComponent />
+      {customTrigger}
+      {isDesktop ? (
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
+          <AnimatePresence>
+            <DialogPortal forceMount>
+              {isOpen && (
+                <DialogContent className="border-none bg-transparent p-0">
+                  <motion.div
+                    layoutId="customModal"
+                    transition={springTransition}
+                    className="rounded-lg border bg-white p-4 shadow-lg dark:bg-slate-950"
+                  >
+                    <DialogClose />
+                    <DialogHeader>
+                      <DialogTitle>Are you absolutely sure?</DialogTitle>
+                      <DialogDescription>
+                        This action cannot be undone. Are you sure you want to
+                        permanently delete this file from our servers?
+                      </DialogDescription>
+                    </DialogHeader>
+                  </motion.div>
+                </DialogContent>
+              )}
+            </DialogPortal>
+          </AnimatePresence>
+        </Dialog>
+      ) : (
+        <Drawer open={isOpen} onOpenChange={setIsOpen}>
+          <DrawerContent>
+            <DrawerHeader>
+              <DrawerTitle>Edit Ingredient</DrawerTitle>
+            </DrawerHeader>
+            <div className="px-4 pb-4">
+              <p>
+                This action cannot be undone. Are you sure you want to
+                permanently delete this file from our servers?
+              </p>
+            </div>
+          </DrawerContent>
+        </Drawer>
+      )}
     </div>
   );
 };
---
+++ src/components/ui/combobox.tsx
@@ -0,0 +1,203 @@
+import * as React from "react";
+import { cn } from "~/lib/utils";
+import { Check, ChevronsUpDown, X } from "lucide-react";
+
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+
+export type OptionType = Record<"value" | "label", string>;
+
+interface ComboboxProps {
+  options: Record<"value" | "label", string>[];
+  selected: Record<"value" | "label", string>[];
+  onChange: (value: OptionType[]) => void;
+  isMultiSelect?: boolean;
+  allowsCustomValue?: boolean;
+  className?: string;
+  placeholder?: string;
+  isModal?: boolean;
+}
+
+const Combobox = React.forwardRef<HTMLButtonElement, ComboboxProps>(
+  (
+    {
+      options,
+      selected,
+      onChange,
+      isMultiSelect = true,
+      allowsCustomValue = false,
+      className,
+      isModal = false,
+      ...props
+    },
+    ref,
+  ) => {
+    const [open, setOpen] = React.useState(false);
+    const [searchValue, setSearchValue] = React.useState("");
+
+    const handleUnselect = (item: Record<"value" | "label", string>) => {
+      onChange(selected.filter((i) => i.value !== item.value));
+    };
+
+    // on delete key press, remove last selected item
+    React.useEffect(() => {
+      const handleKeyDown = (e: KeyboardEvent) => {
+        // if (e.key === "Backspace" && selected.length > 0) {
+        //   onChange(
+        //     selected.filter((_, index) => index !== selected.length - 1),
+        //   );
+        // }
+
+        // close on escape
+        if (e.key === "Escape") {
+          e.stopPropagation();
+          e.stopImmediatePropagation();
+          setOpen(false);
+        }
+      };
+
+      document.addEventListener("keydown", handleKeyDown);
+
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, [onChange, selected]);
+
+    console.log(isModal);
+
+    return (
+      <Popover open={open} onOpenChange={setOpen} modal={isModal}>
+        <PopoverTrigger asChild className={className}>
+          <Button
+            ref={ref}
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className={`group w-full justify-between ${
+              selected.length > 1 ? "h-full" : "h-10"
+            }`}
+            onClick={() => setOpen(!open)}
+          >
+            <>
+              <div className="flex flex-wrap items-center gap-1">
+                {isMultiSelect
+                  ? selected.map((item) => (
+                      <Badge
+                        variant="outline"
+                        key={item.value}
+                        className="flex items-center gap-1 group-hover:bg-background"
+                        onClick={() => handleUnselect(item)}
+                      >
+                        {item.label}
+                        <Button
+                          asChild
+                          variant="outline"
+                          size="badgeIcon"
+                          className="border-none"
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                              handleUnselect(item);
+                            }
+                          }}
+                          onMouseDown={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                          }}
+                          onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            handleUnselect(item);
+                          }}
+                        >
+                          <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
+                        </Button>
+                      </Badge>
+                    ))
+                  : selected.map((item, i) => (
+                      <p key={`${item.value}_${i}`}>{item.label}</p>
+                    ))}
+                {selected.length === 0 && (
+                  <span>{props.placeholder ?? "Select ..."}</span>
+                )}
+              </div>
+              <ChevronsUpDown className="h-4 w-4 shrink-0 opacity-50" />
+            </>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full p-0">
+          <Command className={className}>
+            <CommandInput
+              value={searchValue}
+              onValueChange={setSearchValue}
+              placeholder="Search ..."
+            />
+            <CommandList>
+              <CommandEmpty>
+                {allowsCustomValue ? (
+                  <p
+                    className="cursor-pointer p-1"
+                    onClick={() =>
+                      onChange([
+                        {
+                          label: searchValue,
+                          value: searchValue.toLowerCase(),
+                        },
+                      ])
+                    }
+                  >
+                    <span className="font-bold">Click to add: </span>
+                    {searchValue}
+                  </p>
+                ) : (
+                  <>No item found.</>
+                )}
+              </CommandEmpty>
+              <CommandGroup className="max-h-64 overflow-auto">
+                {options.map((option) => (
+                  <CommandItem
+                    key={option.value}
+                    onSelect={() => {
+                      onChange(
+                        isMultiSelect
+                          ? selected.some((item) => item.value === option.value)
+                            ? selected.filter(
+                                (item) => item.value !== option.value,
+                              )
+                            : [...selected, option]
+                          : [option],
+                      );
+                      !isMultiSelect && setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        selected.some((item) => item.value === option.value)
+                          ? "opacity-100"
+                          : "opacity-0",
+                      )}
+                    />
+                    {option.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    );
+  },
+);
+
+Combobox.displayName = "Combobox";
+
+export { Combobox };
