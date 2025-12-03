526bb37686d9117cd306b7df9d14dacb59a9454e
Tyler Pashigian <tylerpashigian@gmail.com>
2025-12-03 07:49:51 -0600

    Fixing mobile breakpoint on pages list page

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -2,7 +2,7 @@ import ServerRouteList from "../_components/server-routes";
 
 export default async function Pages() {
   return (
-    <div className="container mx-auto flex flex-col justify-center gap-12 px-80 py-16">
+    <div className="container mx-auto flex flex-col items-center justify-center gap-12 py-16 text-center">
       <h1 className="mb-6 text-2xl font-bold">UI Sandbox Pages</h1>
       <ServerRouteList />
     </div>
